e6b9b8f9c0728ad42b903e904934244bf80cef22
dltjdwns331 <79697479+dltjdwns331@users.noreply.github.com>
2022-04-08 14:49:16 +0900

    member code
    회원 정보 관련 임시 코드

---
+++ manage.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+  <head>
+    <meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+    <title>회원정보수정</title>
+  </head>
+  <body>
+   <?php
+	# DB 연결하기
+include_once('dbconn.php');
+# 데이터 가져오기
+	$name = $_POST["name"];
+	$phone1 = $_POST["phone"];
+	$phone2 = $_POST["phone2"];
+	$phone3 = $_POST["phone3"];
+	$address = $_POST["address"];
+          $id = $_POST[“id”];
+          $password =$_POST[“password”];
+	$phone = $phone1."-".$phone2."-".$phone3;
+	# SQL 작성하기
+$sql = "insert into member_info values('$name','$phone','$address',‘$id’,‘password’)";
+# SQL 실행하기
+if($conn->query($sql)) {
+	echo "$name 회원님 환영합니다.<br>";
+	echo "<a href='memeber.php'></a>";
+}
+else 
+	echo "회원가입 중에 오류가 발생하였습니다.". $conn->error;
+?>
---
+++ member.php
@@ -0,0 +1,49 @@
+<!DOCTYPE HTML PUBLIC >
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=euc-kr">
+<title>member join</title>
+
+
+
+
+</style>
+</head>
+
+<body>
+<form action="member_save.php" method='post' name='form2'>
+
+<table border=1 align="center" cellpadding=2 cellspacing=1 bgcolor=#FF9D9D>
+<tr>
+
+<tr><td><center>
+
+<table border=1 cellspacing=3 cellpadding=2 width=450 bgcolor=#FFD0D0>
+<tr> #이름
+   <th width=70 height=20><img src="name.jpg" width="90" height="20"></th>
+   <td width=300 height=20 align="left"><input type='text' name='name' size=10 maxlength=5>
+<tr> #핸드폰 번호
+    <th width=70 height=20><img src="phone_number.jpg" width="90" height="20"></th>
+    <td width=300 height=20 align="left"><input type='text' name='phone' size=3 maxlength=30> <span class="style1">-</span>    <input type='text' name='phone2' size=5 maxlength=30> <span class="style1">-</span>    <input type='text' name='phone3' size=5 maxlength=30></td>
+<tr>    #주소
+    <th width=70 height=20><img src="address.jpg" width="90" height="20"></th>
+	<td width=300 height=20 align="left"><input type='text' name='address' size=50 maxlength=50></td>
+	</tr>
+<tr>      #아이디
+    <th width=70 height=20><img src="id.jpg" width="90" height="20"></th>
+	<td width=300 height=20 align="left"><input type='text' name='id' size=50 maxlength=50></td>
+	</tr>
+<tr>       #비번 
+    <th width=70 height=20><img src="password“.jpg" width="90" height="20"></th>
+	<td width=300 height=20 align="left"><input type='text' name='password' size=50 maxlength=50></td>
+	</tr>
+</table>
+<input type="submit" value="join">
+</center>
+</td>
+
+</table>
+
+</form>
+</body>
+</html>
