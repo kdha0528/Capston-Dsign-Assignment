6e84a119986e64e3703235090009b07ed1a6ad86
dltjdwns331 <79697479+dltjdwns331@users.noreply.github.com>
2022-04-29 15:30:14 +0900

    signup_save.php 생성
    회원가입의 기능적인 부분을 다루는 php파일을 생성하였습니다.

--- dbconn.php
+++ dbconn.php
@@ -1,6 +1,6 @@
 <?php
 #mysql서버의 특정데이터베이스로 연결하기 
-$server = "momentcinema_db";//mysql서버가 동작하는 호스트 컴퓨터의 ip주소 또는 도메인명
+$server = "database-1.cgar2fdlk4le.us-west-1.rds.amazonaws.com";//mysql서버가 동작하는 호스트 컴퓨터의 ip주소 또는 도메인명
 $user = "daejin";        //mysql서버의 접속 계정 
 $passwd = "daejin1234";    //비밀번호  #(root) 
 $dbname = "momentcinema_db"; //연결할 데이터베이스명
---
+++ signup_save.php
@@ -0,0 +1,38 @@
+<!DOCTYPE HTML PUBLIC >
+<head>
+<meta charset="utf-8">
+<meta http-equiv="Content-Type"  content="text/html" /> 
+ 
+<title>회원정보 저장</title>
+</head>
+
+
+<?php
+	# DB 연결하기
+	
+include_once('dbconn.php');
+# 데이터 가져오기
+	
+	
+    $id = $_POST['id'];
+    $password =$_POST['password'];
+	$name = $_POST['name'];
+	$phone = $_POST['phone'];
+	$age = $_POST['age'];
+	$point = 0;
+	date_default_timezone_set("Asia/Seoul");
+	$date = date("Y/m/d");
+	$email = $_POST['email'];
+	
+
+	# SQL 작성하기
+	$sql = "insert into users values('$id','$password','$name','$phone','$age',$point,'$date','$email')";
+# SQL 실행하기
+if($conn->query($sql)) {
+	echo "<script type='text/javascript'>alert('회원가입에 성공했습니다.');</script>"
+	echo "<script>location.href='MomentCinema.html'</script>";
+}else {
+	echo "<script type='text/javascript'>alert('회원가입 중에 오류가 발생하였습니다. $conn->error');</script>";
+	echo "<script>location.href='signup.html'</script>";
+}
+?>
