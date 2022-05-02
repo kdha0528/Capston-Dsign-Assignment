17a18ab2da2d4e507bcb398bb7910b146e905ec7
dltjdwns331 <79697479+dltjdwns331@users.noreply.github.com>
2022-05-02 16:29:29 +0900

    회원가입수정
    회원가입수정

--- signup_save.php
+++ signup_save.php
@@ -19,21 +19,24 @@ include_once('dbconn.php');
 	$name = $_POST["name"];
 	$phone = $_POST["phone"];
 	$age = $_POST["age"];
-	#date_default_timezone_set("Asia/Seoul");
-	$date = date("Y/m/d");
+
+	//date_default_timezone_set("Asia/Seoul");
+	//$date = date("Y/m/d");
 	$email = $_POST["email"];
 	
 	
 
 	# SQL 작성하기
-$sql = "insert into users values('$id','$password','$name','$phone','$age','$date','$email')";
+$sql = "insert into users values('$id','$password','$name','$phone','$age','$email')";
 # SQL 실행하기
 if($conn->query($sql)) {
 	echo "<script>alert('$name 회원의 신규가입이 완료되었습니다')</script>";
 	echo "<script>location.replace('momentCinema.html');</script>";
 }
+
 else {
 	echo "<script>alert('회원가입중 오류가 발생했습니다')</script>";
+	echo "DB 연결에 실패했습니다 " . mysqli_connect_error();
 	echo "<script>location.replace('momentCinema.html');</script>";
 }
 ?>
